8462d2db8a5d0beb928fe37bc525fe875aaa2bb5
Miguel <mad_morla@hotmail.com>
2022-05-19 10:54:38 +0200

    Añade y configura un linter de tu elección al proyecto Node con una configuración básica, por ejemplo: https://github.com/airbnb/javascript

--- Server/server.js
+++ Server/server.js
@@ -1,19 +1,18 @@
-
 const express = require('express');
+
 const app = express();
 
 const date = require('date-and-time');
 
 // Routes
-app.get("/", async (req, res) => {
-    const now = new Date();
-    var currentHour = date.format(now, 'hh:mm A [GMT]Z'); 
-
-    res.send(currentHour);
+app.get('/', async (req, res) => {
+  const now = new Date();
+  const currentHour = date.format(now, 'hh:mm A [GMT]Z');
+  res.send(currentHour);
 });
 
 // Start server listening
 const port = 8000;
 app.listen(port, () => {
-    console.log("Server listening on " + port);
+  console.log(`Server listening on ${port}`);
 });
